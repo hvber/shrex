9f501901126f7bb98c0a85acf92e89fdf8eb127e
Sebastian <51960484+hvber@users.noreply.github.com>
2020-02-29 22:21:16 +0100

    Add files via upload

--- js/memes.js
+++ js/memes.js
@@ -1,5 +1,29 @@
-function like(){
-    let heart = document.querySelector('.fa-heart');
-    
-    heart.classList.toggle('.active');
+var checked=0;
+var checked2=0;
+
+var property = document.querySelector('.meme-btn');
+var property2 = document.querySelector('.meme-btn2');
+function setColor(){
+    if(checked == 1){
+        property.style.color="white";
+        checked = 0;
+    } 
+    else{
+        property.style.color="red";
+        property2.style.color="white";
+        checked = 1;
+        checked2 = 0;
+    }
+}
+function setColor2(){
+    if(checked2 == 1){
+        property2.style.color="white";
+        checked2 = 0;
+    } 
+    else{
+        property2.style.color="red";
+        checked = 0;
+        property.style.color="white";
+        checked2 = 1;
+    }
 }
